ac614f8ea0d30b6bdf876660d2d32b95eafc5908
Dan Phillimore <dan@ovms.co>
2023-07-28 00:43:27 +0100

    Only raise a warning when ext-amqp is also installed, to allow gradual adoption

--- src/AmqpCompat/bootstrap.php
+++ src/AmqpCompat/bootstrap.php
@@ -13,8 +13,13 @@ declare(strict_types=1);
 
 use Asmblah\PhpAmqpCompat\Bridge\AmqpBridge;
 
-if (extension_loaded('amqp')) {
-    throw new RuntimeException('ext-amqp must be uninstalled to use php-amqp-compat');
+if (extension_loaded('amqp') && !defined('PHP_AMQP_COMPAT')) {
+    trigger_error(
+        'ext-amqp must be uninstalled to use php-amqp-compat, ext-amqp will still be used',
+        E_USER_WARNING
+    );
+
+    return;
 }
 
 // Include global AMQP constants.
