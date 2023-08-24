d8e7ca4b1f7f976c888c5dd675ec611520012999
Dan Phillimore <dan@ovms.co>
2023-08-24 10:54:43 +0100

    Run all currently supported reference implementation tests
    Switches to an exclusion-list approach instead

--- tests/Functional/Reference/ReferenceImplementationTest.php
+++ tests/Functional/Reference/ReferenceImplementationTest.php
@@ -26,11 +26,114 @@ use Generator;
  */
 class ReferenceImplementationTest extends AbstractTestCase
 {
+    public const UNSUPPORTED_TESTS = [
+        '003-channel-consumers.phpt',
+        '004-queue-consume-nested.phpt',
+        '004-queue-consume-orphaned.phpt',
+        'amqpbasicproperties.phpt',
+        'amqpchannel_basicRecover.phpt',
+        'amqpchannel_close.phpt',
+        'amqpchannel_confirmSelect.phpt',
+        'amqpchannel_construct_ini_global_prefetch_count.phpt',
+        'amqpchannel_construct_ini_global_prefetch_size.phpt',
+        'amqpchannel_construct_ini_prefetch_count.phpt',
+        'amqpchannel_construct_ini_prefetch_size.phpt',
+        'amqpchannel_getChannelId.phpt',
+        'amqpchannel_multi_channel_connection.phpt',
+        'amqpchannel_set_global_prefetch_count.phpt',
+        'amqpchannel_set_global_prefetch_size.phpt',
+        'amqpchannel_set_prefetch_and_global_prefetch_count.phpt',
+        'amqpchannel_set_prefetch_and_global_prefetch_size.phpt',
+        'amqpchannel_set_prefetch_count.phpt',
+        'amqpchannel_set_prefetch_size.phpt',
+        'amqpchannel_var_dump.phpt',
+        'amqpconnection_heartbeat.phpt',
+        'amqpconnection_heartbeat_with_consumer.phpt',
+        'amqpconnection_heartbeat_with_persistent.phpt',
+        'amqpconnection_persistent_construct_basic.phpt',
+        'amqpconnection_persistent_in_use.phpt',
+        'amqpconnection_persistent_reusable.phpt',
+        'amqpconnection_setSaslMethod.phpt',
+        'amqpconnection_setSaslMethod_invalid.phpt',
+        'amqpconnection_setTimeout_deprecated.phpt',
+        'amqpconnection_setTimeout_float.phpt',
+        'amqpconnection_setTimeout_int.phpt',
+        'amqpconnection_setTimeout_out_of_range.phpt',
+        'amqpconnection_setTimeout_string.phpt',
+        'amqpconnection_toomanychannels.phpt',
+        'amqpconnection_var_dump.phpt',
+        'amqpenvelope_construct.phpt',
+        'amqpenvelope_get_accessors.phpt',
+        'amqpenvelope_var_dump.phpt',
+        'amqpexchange_bind_with_arguments.phpt',
+        'amqpexchange_bind_without_key.phpt',
+        'amqpexchange_bind_without_key_with_arguments.phpt',
+        'amqpexchange_delete_default_exchange.phpt',
+        'amqpexchange_get_channel.phpt',
+        'amqpexchange_invalid_type.phpt',
+        'amqpexchange_publish_confirms.phpt',
+        'amqpexchange_publish_confirms_consume.phpt',
+        'amqpexchange_publish_mandatory.phpt',
+        'amqpexchange_publish_mandatory_consume.phpt',
+        'amqpexchange_publish_mandatory_multiple_channels_pitfal.phpt',
+        'amqpexchange_publish_with_decimal_header.phpt',
+        'amqpexchange_publish_with_null.phpt',
+        'amqpexchange_publish_with_properties.phpt',
+        'amqpexchange_publish_with_properties_ignore_num_header.phpt',
+        'amqpexchange_publish_with_properties_ignore_unsupported_header_values.phpt',
+        'amqpexchange_publish_with_properties_nested_header.phpt',
+        'amqpexchange_publish_with_properties_user_id_failure.phpt',
+        'amqpexchange_publish_with_timestamp_header.phpt',
+        'amqpexchange_publish_xdeath.phpt',
+        'amqpexchange_setArgument.phpt',
+        'amqpexchange_set_flags.phpt',
+        'amqpexchange_unbind_with_arguments.phpt',
+        'amqpexchange_unbind_without_key_with_arguments.phpt',
+        'amqpexchange_var_dump.phpt',
+        'amqpqueue-cancel-no-consumers.phpt',
+        'amqpqueue_bind_basic_headers_arguments.phpt',
+        'amqpqueue_cancel.phpt',
+        'amqpqueue_consume_basic.phpt',
+        'amqpqueue_consume_multiple.phpt',
+        'amqpqueue_consume_multiple_no_doubles.phpt',
+        'amqpqueue_consume_nonexistent.phpt',
+        'amqpqueue_consume_timeout.phpt',
+        'amqpqueue_empty_name.phpt',
+        'amqpqueue_get_basic.phpt',
+        'amqpqueue_get_channel.phpt',
+        'amqpqueue_get_empty_body.phpt',
+        'amqpqueue_get_headers.phpt',
+        'amqpqueue_get_nonexistent.phpt',
+        'amqpqueue_headers_with_bool.phpt',
+        'amqpqueue_headers_with_float.phpt',
+        'amqpqueue_headers_with_null.phpt',
+        'amqpqueue_nack.phpt',
+        'amqpqueue_nested_arrays.phpt',
+        'amqpqueue_nested_headers.phpt',
+        'amqpqueue_setArgument.phpt',
+        'amqpqueue_unbind_basic_empty_routing_key.phpt',
+        'amqpqueue_unbind_basic_headers_arguments.phpt',
+        'amqpqueue_var_dump.phpt',
+        'amqptimestamp_php8.phpt',
+        'bug_19707.phpt',
+        'bug_62354.phpt',
+        'bug_gh147.phpt',
+        'bug_gh155_direct_reply_to.phpt',
+        'bug_gh53-2.phpt',
+        'bug_gh53.phpt',
+        'bug_gh72-2.phpt',
+        'package-version.phpt',
+    ];
+
     /**
      * @dataProvider referenceImplementationTestProvider
      */
     public function testReferenceImplementationTest(string $path): void
     {
+        if (in_array(basename($path), self::UNSUPPORTED_TESTS, true)) {
+            $this->markTestSkipped('Not yet supported');
+        }
+
         $runnerArguments = [];
         $testArguments = [
             // Load our bootstrap script before executing .phpt tests.
@@ -89,11 +192,7 @@ class ReferenceImplementationTest extends AbstractTestCase
     {
         $basePath = realpath(__DIR__ . '/../../../var/ext/php-amqp/tests');
 
-        // TODO: Have all possible tests passing with `glob($basePath . '/*.phpt')`.
-        $phptFiles = glob(
-            $basePath . '/{amqpconnection_construct_*,amqpconnection_connect_login_failure,amqpconnection_getUsedChannels,amqpconnection_connection_getters,amqpdecimal,amqpexchange_declare*,amqpqueue_declare*}.phpt',
-            GLOB_BRACE
-        );
+        $phptFiles = glob($basePath . '/*.phpt', GLOB_BRACE);
 
         foreach ($phptFiles as $path) {
             yield basename($path) => [$path];
